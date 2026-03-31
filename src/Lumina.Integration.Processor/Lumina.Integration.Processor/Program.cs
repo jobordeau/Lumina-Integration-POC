46930feeb5e4bda9b884a10c02e0e57a24cf863a
jobordeau <74385376+jobordeau@users.noreply.github.com>
2026-04-01 01:57:39 +0200

    add ApplicationInsights

--- src/Lumina.Integration.Processor/Lumina.Integration.Processor/Program.cs
+++ src/Lumina.Integration.Processor/Lumina.Integration.Processor/Program.cs
@@ -1,11 +1,15 @@
-using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Lumina.Integration.Processor.Core.Interfaces;
 using Lumina.Integration.Processor.Core.Services;
 using Lumina.Integration.Processor.Infrastructure.Adapters;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationInsightsTelemetryWorkerService();
+builder.Services.ConfigureFunctionsApplicationInsights();
 
 builder.Services.AddScoped<IOrderProcessingService, OrderProcessingService>();
 builder.Services.AddScoped<IOrderRepository, DataLakeOrderRepository>();
