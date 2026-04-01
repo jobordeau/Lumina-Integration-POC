dc861dbc39d3ab23b83aa051eb3313882602d141
jobordeau <74385376+jobordeau@users.noreply.github.com>
2026-04-01 03:27:50 +0200

    fix: 503 error

--- src/Lumina.Integration.Processor/Lumina.Integration.Processor/Program.cs
+++ src/Lumina.Integration.Processor/Lumina.Integration.Processor/Program.cs
@@ -2,16 +2,18 @@ using Lumina.Integration.Processor.Core.Interfaces;
 using Lumina.Integration.Processor.Core.Services;
 using Lumina.Integration.Processor.Infrastructure.Adapters;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddScoped<IOrderProcessingService, OrderProcessingService>();
+        services.AddScoped<IOrderRepository, DataLakeOrderRepository>();
+    })
+    .Build();
 
-builder.Services.AddApplicationInsightsTelemetryWorkerService();
-builder.Services.ConfigureFunctionsApplicationInsights();
-
-builder.Services.AddScoped<IOrderProcessingService, OrderProcessingService>();
-builder.Services.AddScoped<IOrderRepository, DataLakeOrderRepository>();
-
-builder.Build().Run();
+await host.RunAsync();
